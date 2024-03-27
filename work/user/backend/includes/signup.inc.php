21317d1d2ac1f4d0c979cfcfdf1771cd4cfbd8ef
tasnimmizaou <tasnim.mizaoui@insat.ucar.tn>
2024-03-27 22:47:49 +0100

    ...

--- work/user/backend/includes/signup.inc.php
+++ work/user/backend/includes/signup.inc.php
@@ -1,25 +1,53 @@
 <?php
-if($_SERVER ["REQUEST_METHOD"] == "POST"){
-
-
-}else{ 
+if($_SERVER ["REQUEST_METHOD"] === 'POST'){
+  try{
        $username=$_POST["username"];
        $password=$_POST["password"];
        $email=$_POST["email"];
        $phone=$_POST["phone"];
        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-       require_once "dbh.inc.php";
-     $query="INSERT INTO users( username,password,email,phone) VALUES(:username,:password,:email,::phone);"; 
-     $stmt=$pdo->prepare($query);
-     $stmt->bindParam(":username",$username);
-     $stmt->bindParam(":password",$hashedPassword);
-     $stmt->bindParam(":email",$email);
-     $stmt->bindParam(":phone",$phone);
 
-     $stmt->execute();
-     $pdo=null;
-     $stmt=null;
+       require_once "dbh.inc.php";
+       require_once "signup_model.inc.php";
+       //require_once  "signup_view.inc.php";
+       require_once "signup_cont.inc.php";
+      
+        //Error Handling:
+        $errors=[];
+        if ( is_input_empty($username,$password,$phone,$email) ){
+          $errors["empty_input"]="Please enter all fields !!";
+        }
+
+        if (is_email_invalid($email)){
+          $errors["invalid_email"]="Please enter a valid email!!";
+        }
+
+        if (is_username_used($pdo ,$username)){
+          $errors["used_username"]="This username is already taken !!";
+        }
+
+        if (is_email_registered($pdo ,$email))  {
+          $errors["used_email"]="This E-mail is already used !!";
+        }
+
+        require_once 'configSession.inc.php';
+       if ($errors){
+          $_SESSION["errors_signup"] =$errors;
+          header("location:../index.php");
+       } 
+
+
      header("location:../index.php");
      die();
+    
+     }
+      catch(PDOException $e)
+     {
+      die ("Error: " . $e->getMessage());
+     }
 
+
+} else{
+  header("location:../index.php");
+  die();
 }
