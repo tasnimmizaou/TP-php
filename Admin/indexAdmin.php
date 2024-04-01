7799cd6b3629101be926f37772f3d152e3cab595
eya2105 <97851315+eya2105@users.noreply.github.com>
2024-04-01 21:53:38 +0100

    Update indexAdmin.php

--- Admin/indexAdmin.php
+++ Admin/indexAdmin.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styleAdmin.css">
     <title>Login Admin</title>
 </head>
 <body>
-    <form action="login.php" method="post">
+    <form action="loginAdmin.php" method="post">
         <h2>Login Admin</h2>
         <?php if (isset($_GET['error'])){ ?>
             <p class="error"><?php echo $_GET['error'];?></p>
