24d9fba8a7fc06059c6cd84a06cd96cb8e4302fb
farah246 <157645571+farah246@users.noreply.github.com>
2024-04-02 16:41:14 +0100

    Update ajouterAdmin.php

--- Admin/ajouterAdmin.php
+++ Admin/ajouterAdmin.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Dashboard d'administration_Ajouter un admin</title>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
-<div class="container">
+<?php include('header.php'); include('navbar.php'); ?>
+<a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+</a>
+<div class="container-fluid">
     <h2 class="mt-5 mb-4">Ajouter un admin</h2>
     <form action="" method="post" enctype="multipart/form-data"> <!-- Ajout de enctype pour gérer les fichiers -->
         <div class="form-group">
@@ -26,7 +22,6 @@
     </form>
 </div>
 <?php
-// Traitement du formulaire lorsqu'il est soumis
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST['username'], $_POST['userpassword'], $_POST['email'])) {
         $username = $_POST['username'];
@@ -38,16 +33,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $req = $pdo->prepare("INSERT INTO admins (username,userpassword, email) VALUES (?, ?, ?)");
 
         if ($req->execute([$username, $userpassword, $email])) {
-            echo "<div class='alert alert-success mt-3' role='alert'>L'admin a été ajouté avec succès.</div>";
+            $_SESSION['success']="Admin ajoute avec succes";
+            header('location: tableAdmin.php');
         } else {
-            echo "<div class='alert alert-danger mt-3' role='alert'>Erreur lors de l'ajout de l'admin.</div>";
+            $_SESSION['status']="Admin non ajouté";
         }
     } else {
-        echo "<div class='alert alert-warning mt-3' role='alert'>Veuillez fournir toutes les données nécessaires.</div>";
+        $_SESSION['status']="Veuillez fournir toutes les données nécessaires";
+
     }
 }
 ?>
 
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+</a>
 </body>
+<?php include("footer.php"); include("scripts.php");?>
 </html>
