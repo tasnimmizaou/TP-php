aa35a61ce1114c0c92a1538067643a3dabc42764
farah246 <157645571+farah246@users.noreply.github.com>
2024-04-04 01:49:15 +0100

    Update buttonsCommande.js

--- Admin/buttonsCommande.js
+++ Admin/buttonsCommande.js
@@ -3,10 +3,8 @@ deleteButtons.forEach(button => {
     button.addEventListener('click', function() {
         let commandeId = this.getAttribute('data-id');
         if (confirm('Êtes-vous sûr de vouloir supprimer cette commande ?')) {
-            // Supprimer la ligne de la commande dans le tableau
-            let row = this.parentNode.parentNode; // Récupérer la ligne parente du bouton
-            row.parentNode.removeChild(row); // Supprimer la ligne du tableau
-            // Envoyer une requête AJAX pour supprimer la commande du côté serveur
+=            let row = this.parentNode.parentNode; 
+            row.parentNode.removeChild(row); 
             let xhr = new XMLHttpRequest();
             xhr.open('POST', 'supprimerCommande.php', true);
             xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
