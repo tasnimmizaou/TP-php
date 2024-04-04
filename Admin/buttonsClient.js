97fee3f4784131f5ee770fe81b9c29653085e0d3
farah246 <157645571+farah246@users.noreply.github.com>
2024-04-04 01:49:40 +0100

    Update buttonsClient.js

--- Admin/buttonsClient.js
+++ Admin/buttonsClient.js
@@ -3,10 +3,8 @@ deleteButtons.forEach(button => {
     button.addEventListener('click', function() {
         let clientId = this.getAttribute('data-id');
         if (confirm('Êtes-vous sûr de vouloir supprimer ce client ?')) {
-            // Supprimer la ligne duclient dans le tableau
-            let row = this.parentNode.parentNode; // Récupérer la ligne parente du bouton
-            row.parentNode.removeChild(row); // Supprimer la ligne du tableau
-            // Envoyer une requête AJAX pour supprimer le client du côté serveur
+            let row = this.parentNode.parentNode; 
+            row.parentNode.removeChild(row);
             let xhr = new XMLHttpRequest();
             xhr.open('POST', 'supprimerClient.php', true);
             xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
