418d29d7bc74c023399a7dbc204b14cb7ce09c74
farah246 <157645571+farah246@users.noreply.github.com>
2024-04-04 20:29:23 +0100

    Update index.php

--- Admin/index.php
+++ Admin/index.php
@@ -1,37 +1,26 @@
 
 <?php include ('header.php'); include ('navbar.php'); ?>
 
-<!-- Content Wrapper -->
 <div id="content-wrapper" class="d-flex flex-column">
 
-    <!-- Main Content -->
     <div id="content">
 
-        <!-- Topbar -->
         <?php include('nav.php');?>
-        <!-- End of Topbar -->
 
-        <!-- Begin Page Content -->
         <div class="container-fluid">
 
-            <!-- Page Heading -->
             <div class="d-sm-flex align-items-center justify-content-between mb-4">
                 <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
             </div>
 
-            <!-- Content Row -->
             <div class="row">
-                <!-- Pending Requests Card Example -->
 
             </div>
 
-            <!-- Content Row -->
 
 
 
         </div>
-        <!-- /.container-fluid -->
 
     </div>
-    <!-- End of Main Content -->
 <?php include('scripts.php'); include('footer.php');
