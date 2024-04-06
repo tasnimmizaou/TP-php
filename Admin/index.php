8afa111f8f3b2b6af3c4ddc5e471c63566621cc7
farah246 <157645571+farah246@users.noreply.github.com>
2024-04-06 17:46:14 +0100

    Update index.php

--- Admin/index.php
+++ Admin/index.php
@@ -1,26 +1,56 @@
+<?php
+include('header.php');
+include('navbar.php');
+include("generateChart.php");
+?>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script src="generateChart.js"></script>
+<style>
+    .chart-container {
+        width: 300px;
+        height: 300px;
+    }
 
-<?php include ('header.php'); include ('navbar.php'); ?>
-
+    .chart-wrapper {
+        display: inline-block;
+        margin-right: 20px;
+    }
+</style>
 <div id="content-wrapper" class="d-flex flex-column">
-
     <div id="content">
+        <?php include('nav.php'); ?>
 
-        <?php include('nav.php');?>
-
-        <div class="container-fluid">
-
-            <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+        <div class="chart-wrapper">
+            <h5 class="text-center">Charts des catégories</h5>
+            <div class="chart-container">
+                <canvas id="myChart1"></canvas>
             </div>
+            <?php
+            $query1 = "SELECT category, COUNT(*) AS count FROM article INNER JOIN details_commande ON article.id = details_commande.article_id GROUP BY category";
+            $chartData1 = generateChartData($query1, 'category', 'count');
+            ?>
+            <script>
+                const ctx1 = document.getElementById('myChart1');
+                generateChart(ctx1, 'pie', <?php echo json_encode($chartData1); ?>, 'Nombre d\'articles vendus par catégorie');
+            </script>
+        </div>
 
-            <div class="row">
-
+        <div class="chart-wrapper">
+            <h5 class="text-center">Charts des âges</h5>
+            <div class="chart-container">
+                <canvas id="myChart2"></canvas>
             </div>
-
-
-
-
+            <?php
+            $query2 = "SELECT age, COUNT(*) AS count FROM article INNER JOIN details_commande ON article.id = details_commande.article_id GROUP BY age";
+            $chartData2 = generateChartData($query2, 'age', 'count');
+            ?>
+            <script>
+                const ctx2 = document.getElementById('myChart2');
+                generateChart(ctx2, 'pie', <?php echo json_encode($chartData2); ?>, 'Nombre d\'articles vendus par âge');
+            </script>
         </div>
 
     </div>
-<?php include('scripts.php'); include('footer.php');
+</div>
+
+<?php include("footer.php"); ?>
