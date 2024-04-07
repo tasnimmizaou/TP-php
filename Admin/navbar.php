2e631501c955216d451c6ea906e445425d6eff69
farah246 <157645571+farah246@users.noreply.github.com>
2024-04-07 14:14:53 +0100

    Update navbar.php

--- Admin/navbar.php
+++ Admin/navbar.php
@@ -10,7 +10,7 @@
     <hr class="sidebar-divider">
 
     <div class="sidebar-heading">
-        Interface
+
     </div>
 
     <li class="nav-item">
@@ -36,12 +36,6 @@
         </a>
     </li>
 
-    <li class="nav-item">
-        <a class="nav-link" href="charts.html">
-            <i class="fas fa-fw fa-chart-area"></i>
-            <span>Charts</span></a>
-    </li>
-
 
     <hr class="sidebar-divider d-none d-md-block">
 
@@ -53,7 +47,6 @@
 </ul>
 
 
-<!-- Logout Modal-->
 <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
      aria-hidden="true">
     <div class="modal-dialog" role="document">
@@ -67,7 +60,7 @@
             <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
             <div class="modal-footer">
                 <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                <a class="btn btn-primary" href="login.html">Logout</a><!--lina nhot mtaa eya-->
+                <a class="btn btn-primary" href="indexAdmin.php">Logout</a>
             </div>
         </div>
     </div>
