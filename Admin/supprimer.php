44893e6d1f0e09fa418d863fc8afd384351b3e96
farah246 <157645571+farah246@users.noreply.github.com>
2024-04-10 02:40:06 +0100

    Update supprimer.php

--- Admin/supprimer.php
+++ Admin/supprimer.php
@@ -5,7 +5,22 @@ if (isset($_POST['id']) && isset($_POST['table'])) {
 
     require_once 'commun/autoloader.php';
     $pdo = ConnexionBD::getInstance();
-
+    if ($tableName == 'article') {
+        $updateQuery = "UPDATE details_commande SET article_id = NULL WHERE article_id = ?";
+        $stmt = $pdo->prepare($updateQuery);
+        $stmt->execute([$recordId]);
+        $updateQuery2 = "UPDATE panier SET article_id = NULL WHERE article_id = ?";
+        $stmt2 = $pdo->prepare($updateQuery2);
+        $stmt2->execute([$recordId]);
+    }
+    if ($tableName == 'user') {
+        $updateQuery = "UPDATE commande SET user_id = NULL WHERE user_id = ?";
+        $stmt = $pdo->prepare($updateQuery);
+        $stmt->execute([$recordId]);
+        $updateQuery2 = "UPDATE panier SET user_id = NULL WHERE user_id = ?";
+        $stmt2 = $pdo->prepare($updateQuery2);
+        $stmt2->execute([$recordId]);
+    }
     $query = "DELETE FROM $tableName WHERE id = ?";
 
     $stmt = $pdo->prepare($query);
@@ -20,4 +35,5 @@ if (isset($_POST['id']) && isset($_POST['table'])) {
     http_response_code(400);
     echo "ID de l'enregistrement ou nom de la table non spécifié.";
 }
+
 ?>
