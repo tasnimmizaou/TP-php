e8ddc744a29bc96fa330c9cf5758df60eb3946cf
farah246 <157645571+farah246@users.noreply.github.com>
2024-04-20 11:53:06 +0100

    Update ConnexionBD.php

--- Admin/commun/ConnexionBD.php
+++ Admin/commun/ConnexionBD.php
@@ -1,19 +1,22 @@
 <?php
+
 class ConnexionBD
 {
-    private static $_dbname = "postgres";
-    private static $_user = "postgres.wpufhivhhwqurjalucxt";
-    private static $_pwd = "-(2id$7M2p_rJ(Y";
-    private static $_host = "aws-0-eu-central-1.pooler.supabase.com";
+    private static $_dbname = "girlhood";
+    private static $_user = "root";
+    private static $_pwd = "";
+    private static $_host = "localhost";
     private static $_bdd = null;
-    private function __construct()
+
+    public function __construct()
     {
         try {
-            self::$_bdd = new PDO("postgres:host=" . self::$_host . ";dbname=" . self::$_dbname . ";charset=utf8", self::$_user, self::$_pwd,    array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8'));
+            self::$_bdd = new PDO("mysql:host=" . self::$_host . ";dbname=" . self::$_dbname . ";charset=utf8", self::$_user, self::$_pwd, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8'));
         } catch (PDOException $e) {
             die('Erreur : ' . $e->getMessage());
         }
     }
+
     public static function getInstance()
     {
         if (!self::$_bdd) {
@@ -21,4 +24,7 @@ class ConnexionBD
         }
         return (self::$_bdd);
     }
+
+    
 }
+?>
